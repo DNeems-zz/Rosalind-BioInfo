4e228c981743998a9e40f8f1d03436d5471312f3
Daniel Neems <Dneems@gmail.com>
2015-07-22 15:56:09 -0500

    Change to folder Hierarchy, Turned the Support Functions into a Module, updated the ReadMe

---
+++ Exercises/SupportMod/RosSup_Func.py
@@ -0,0 +1,67 @@
+import sys
+def FastaRead(filename):
+  F=open(filename)
+  Names=list()
+  Values=list()
+  for line in F:
+  	if line[0]=='>':
+  		Names.append(line[1:].strip())
+  		Values.append('')
+  	else:
+  		Values[-1]=Values[-1]+line.strip()
+  return [Names,Values]
+
+def FastaWrite(Names,Values,filename='Default.txt',OutputSpace=True):
+	with open(filename, 'a') as f:
+		if type(Names)!=list:
+			Names=[Names]
+		if type(Values)==int or type(Values)==float:
+			Values=[Values]
+		elif type(Values[0])!=list:
+			Values=[Values]
+
+		for n,v in zip(Names,Values):
+		  f.write('>'+n+'\n')
+		  if type(v)==str:
+				f.write(v+'\n')
+		  elif type(v)==list:
+		  	if OutputSpace:
+				f.write(' '.join([str(val) for val in v])+'\n')
+			else:
+				f.write(''.join([str(val) for val in v])+'\n')				
+		  elif type(v)==int or type(v)==float:
+				f.write(str(v)+'\n')
+		f.close()
+
+def Count(DNA):
+  DNABases=['a','c','g','t'];
+  return [DNA.lower().count(nt) for nt in DNABases]
+
+def Transcribe(DNA):
+	return DNA.upper().replace('T','U')
+
+def ReverseComp(DNA):
+	return DNA[::-1].upper().translate(maketrans('ACGT', 'TGCA'))
+
+def Cross(*args):
+	import itertools
+	CrossPairs=list()
+	for Inp in args:
+		Alleles=list(set([a.lower()for a in Inp]))
+		AllelePair=list()
+		for A in Alleles:
+			AllelePair.append('')
+			for I in Inp:
+				if I.lower() == A:
+					AllelePair[-1]=AllelePair[-1]+I
+		CrossPairs.append(list())
+		for i in itertools.product(*AllelePair):
+			CrossPairs[-1].append(''.join(i))
+	Genotypes=list()
+	for i in itertools.product(*CrossPairs):
+		Genotypes.append(''.join(sorted(sorted(''.join(i)), key=str.lower)))
+	uGenotypes=list(set(Genotypes))
+	GenoType_Feq=list()
+	for uG in uGenotypes:
+		GenoType_Feq.append(tuple([uG,Genotypes.count(uG)]))
+	return Genotypes,GenoType_Feq
---
+++ Exercises/Transcribing DNA into RNA-Ex2.py
@@ -0,0 +1,35 @@
+""""
+Transcribing DNA into RNA
+
+Problem
+
+An RNA string is a string formed from the alphabet containing 'A', 'C', 'G', and 'U'.
+Given a DNA string t corresponding to a coding strand, its transcribed RNA string u 
+is formed by replacing all occurrences of 'T' in t with 'U' in u.
+
+Given: A DNA string t having length at most 1000 nt.
+
+Return: The transcribed RNA string of t.
+
+Sample Dataset:
+GATGGAACTTGACTACGTAAATT
+
+Sample Output:
+GAUGGAACUUGACUACGUAAAUU
+"""
+
+import sys
+from RosSupFunc import FastaRead
+from RosSupFunc import FastaWrite
+
+# Define a main() function that prints a little greeting.
+def main():
+  [name,value]=FastaRead(sys.argv[1])
+  Results=list()
+  for entries in value:
+    Results.append(entries.upper().replace('T','U'))
+  FastaWrite(name,Results,'Ex2_Output.txt')
+
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+  main()
